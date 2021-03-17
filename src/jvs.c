c59e8fb1f2fd07397f056e2b87f3a96e0252cb36
Bobby Dilley <bobby@dilley.uk>
2021-03-17 22:47:37 +0000

    fix typo

--- src/jvs.c
+++ src/jvs.c
@@ -381,7 +381,7 @@ JVSStatus readPacket(JVSPacket *packet)
 JVSStatus writePacket(JVSPacket *packet)
 {
 	/* Don't return anything if there isn't anything to write! */
-	if (packet->length < 2)
+	if (packet->length < 1)
 		return JVS_STATUS_SUCCESS;
 
 	/* Get pointer to raw data in packet */
